0dec901dbe04549c5a0aa21b5223a1cce0c4b55e
MenaNabel <mena_mon99@yahoo.com>
2021-03-10 23:45:46 +0200

    Exercise1

---
+++ Exercise1/Exercise1/Controllers/Exercise1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Exercise1.ViewModels;
+using System.Web.Mvc;
+
+namespace Exercise1.Controllers
+{
+    public class Exercise1Controller : Controller
+    {
+        // GET: EX
+        public ActionResult Index()
+        {
+            return View("Index", new HelloWorldCppViewModel());
+        }
+        public ActionResult Run()
+        {
+            return View("Index", new HelloWorldCppViewModel()
+            {  ServerMessage = "Hello World!" });
+        }
+    }
+}
