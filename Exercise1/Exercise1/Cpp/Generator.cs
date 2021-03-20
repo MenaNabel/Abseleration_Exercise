d86e23bea8782be04b247023fbb110c88925fee5
MenaNabel <mena_mon99@yahoo.com>
2021-03-21 00:42:58 +0200

    Add Exercise3

--- Exercise1/Exercise1/Cpp/Generator.cs
+++ Exercise1/Exercise1/Cpp/Generator.cs
@@ -4,28 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Runtime.InteropServices;
 using System.IO;
+using Exercise1.Database;
 
 namespace Exercise1.Cpp
 {
     public class Generator
     {
-
         public static string GenerateCppFile()
         {
+            
             try
             {
-                
+                ProgramsEntities context = new ProgramsEntities();
                 string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 string file = path + @"\TestFile.cpp";
                 TextWriter tw = new StreamWriter(file, true);
-                tw.WriteLine(@"#include <iostream>
-using namespace std;
- 
-int main() {                        
-   cout << ""Hello World!"" << endl;  
-   return 0;
-            }
-");
+                tw.WriteLine(context.Programs.FirstOrDefault().ProgramText);
                 tw.Close();
                 return path;
             }
